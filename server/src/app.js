b354c99398ce3f898e05a841c18b09ed75729b0a
chayada19 <noyhuahatc@gmail.com>
2023-08-23 13:48:21 +0700

    midterm66

--- server/src/app.js
+++ server/src/app.js
@@ -17,6 +17,21 @@ app.get('/hello/:name', function (req,res) {
     res.send('sey hello with ' + req.params.name) 
    })
 
+   app.get('/que1', function (req,res) {
+    res.send('sey hello with ' + req.params.name) 
+   })
+
+   app.get('/que2 ', function isPrime(x) {
+    if (isNaN(n) || !isFinite(n) || n % 1 || n < 2) return false;
+    var m = Math.sqrt(n); 
+    for (var i = 2; i <= m; i++)
+        if (n % i == 0) return false;
+    return true;
+    res.send('sey hello with ' + x) 
+   })
+
+   
+
    
 
 let port = 8081
